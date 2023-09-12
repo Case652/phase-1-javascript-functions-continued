0a9ee43453cbc19848b1f86d607678b240681b11
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-11 20:51:15 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(fun = `roller-skate`) {
+    return `This Saturday, I want to ${fun}!`
+}
+function mondayWork(sad = `go to the office`) {
+    return `This Monday, I will ${sad}.`
+}
+function wrapAdjective(str = `*`) {
+    return function(meter = `special`) {
+        return `You are ${str}${meter}${str}!`
+    }
+}
